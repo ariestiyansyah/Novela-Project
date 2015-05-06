f1e784fdac1313e25dc10859234112dccfea1531
novelaspalo <novela.spalo@gmail.com>
2015-05-06 18:34:29 +0700

    registrasi

---
+++ TodoApp/TodoApp/Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TodoApp.Models;
+namespace TodoApp.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Create
+        [HttpPost]
+        public ActionResult Create(UserRegistration model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    TodoAppDataContext db = new TodoAppDataContext();
+                    User register = new User();
+                    register.id = db.Users.Count() + 1;
+                    register.username = model.Username;
+                    register.password = model.Password;
+                    
+                    db.Users.InsertOnSubmit(register);
+                    db.SubmitChanges(); 
+                    TempData["message"] = "Registration Success";
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TodoApp/TodoApp/Models/UserModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using TodoApp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class UserModels
+    {
+        public void auth(User user)
+        {       
+            
+        }
+    }
+    public class UserRegistration
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string Username
+        {
+            set;
+            get;
+        }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Your password is too short!", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password
+        {
+            set;
+            get;
+        }
+    }
+}
